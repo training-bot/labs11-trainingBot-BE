678b556f869ec39ad4bd25949d9a3ccf7ca8de37
Mike Landers <landers.mike@gmail.com>
2019-09-12 15:22:02 -0700

    added production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,10 @@ module.exports = {
 	development: {
 		client: 'pg',
 		connection: process.env.DATABASE_URL
+	},
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL
 	}
 
 	// development: {
