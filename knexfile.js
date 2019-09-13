6656e605be921ef56e6c42fc661373792919dc42
Mike Landers <landers.mike@gmail.com>
2019-09-13 08:42:27 -0700

    updating knex file

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 module.exports = {
 	development: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL
+		connection: {
+			host: 'localhost',
+			user: 'postgres',
+			password: 'postgres',
+			database: 'postgres'
+		},
 	},
 	production: {
 		client: 'pg',
